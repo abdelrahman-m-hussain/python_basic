83a24a37f83bbef5518ad558acd0d5fa5e071914
EngMostafa <EngMostafa@engineering.com>
2023-09-02 18:38:03 +0300

    basic of datatype

---
+++ Variable_And_DataType.py
@@ -0,0 +1,13 @@
+import datetime
+def main():
+    name=str('Abdo Mostafa')
+    age=int(25)
+    today=datetime.datetime.today()
+    degree=float(2.9)
+    speciality=str('Computer Engineering')
+    print(f'My name is {name},',
+          f'I am {age} years old,',
+          f'My Degree is {degree}/4.0 in {speciality} and',
+          f'the time now is {today:%A %B %d, %Y}' )
+    
+main()
